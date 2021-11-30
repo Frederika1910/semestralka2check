bddca87a2a2a759fb70b4ad124032d3a4c68a0d4
Frederika1910 <91611012+Frederika1910@users.noreply.github.com>
2021-11-30 23:22:07 +0100

    Add files via upload

---
+++ App/Views/Home/aboutus.view.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-sm-12">
+            <img src="https://at-fashion-secondhand.webnode.sk/_files/200000008-ef907ef909/second-hand-clothing-header-070218.jpg?ph=0b51ca5dfb" class="img-fluid" alt="Predajna">
+        </div>
+        <div class="col-lg-9 col-sm-12">
+            <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+            </p>
+        </div>
+        <div class="address text-center">
+            <h4>Kde sídlime?</h4>
+            Radlinského 1720/3<br>
+            Dolný Kubín<br>
+            026 01
+        </div>
+    </div>
+</div>
---
+++ App/Views/Home/loggedUser.view.php
@@ -0,0 +1,38 @@
+<?php /** @var Array $row */ ?>
+<h1 class="text-center">Vítame, Vás vo Vašom účte.</h1>
+
+<div class="row">
+    <div class="col-lg-6 col-md-12">
+        <div class="info">
+            <div class="card user" style="width: 20rem;">
+                <div class="card-body text-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                    </svg>
+
+                    <h5 class="card-title">Moje údaje</h5>
+                    <p class="card-text">Tu si môžete prezrieť a upraviť Vaše údaje.</p>
+                    <a href="?c=auth&a=changePassword" class="btn btn-success mt-2">Upraviť moje údaje</a>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6 col-md-12">
+        <div class="info">
+            <div class="card user" style="width: 20rem;">
+                <div class="card-body text-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                    </svg>
+                    <h5 class="card-title">Moje objednávky</h5>
+                    <p class="card-text">Tu si môžete prezrieť Vaše obejdnávky.</p>
+                    <a href="#" class="btn btn-success mt-2">Prehľad mojich objednávok</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
